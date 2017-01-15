31ec972e787b40f08cc46843a4e06966c46c065c
Tony Polinelli <tonypolinelli@gmail.com>
2017-01-15 17:36:13 +1100

    add multi select example

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    <h3>Selected: {{selectedPerson}}</h3>
-    <x-select
-      :options="[{ id:1, name: 'John' }, { id:2, name: 'Phil' }, { id:3, name: 'paul' }]"
-      v-model="selectedPerson"
-      label-from="name"
-      value-from="id"
-    >
+  <div class="row">
+    <div class="six columns">
+      <h2>Single Select</h2>
+      <h4>Selected: {{selectedPerson}}</h4>
+      <x-select
+        :options="[{ id:1, name: 'John' }, { id:2, name: 'Phil' }, { id:3, name: 'paul' }]"
+        v-model="selectedPerson"
+        label-from="name"
+        value-from="id"
+      >
+      <hr />
+    </div>
+
+    <div class="six columns">
+      <h2>Multi Select</h2>
+      <h4>Selected: {{selectedPeople}}</h4>
+      <x-select
+        :options="[{ id:1, name: 'John' }, { id:2, name: 'Phil' }, { id:3, name: 'paul' }]"
+        v-model="selectedPeople"
+        :multiple="true"
+        label-from="name"
+        value-from="id"
+      >
+    </div>
   </div>
 </template>
 
@@ -20,6 +36,7 @@ export default {
   data() {
     return {
       selectedPerson: 2,
+      selectedPeople: [],
     }
   }
 }
